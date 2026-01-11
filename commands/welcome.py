2ec2369cd927254729cbec62bb2aad519ab1fe96
Jeffrey Lowe <HuntedCode47@gmail.com>
2026-01-10 19:12:21 -0500

    Update welcome message

--- commands/welcome.py
+++ commands/welcome.py
@@ -29,10 +29,10 @@ class WelcomeCog(commands.Cog):
             embed = discord.Embed(title=f"Welcome to Plat Pursuit! {self.bot.plat_pursuit_emoji}", color=0x00ff00, description=f"Hey {mention}! It's good to see you! Follow the instructions below to get the most out of our services.")
             embed.add_field(
                 name='Getting Started',
-                value=f"1. Use **/link <psn_username>** to connect and sync your PSN profile to our service.\n2. Add the generated code to your PSN **'About Me'** and click **Verify Now**.\n3. Once verified you'll have full access to our server and state of the art Discord bot! Feel free to use the various slash commands.\nQuestions? Ping a moderator! {self.bot.plat_pursuit_emoji} {self.bot.platinum_emoji}",
+                value=f"1. Use **/link <psn_username>** to connect and sync your PSN profile to our service.\n2. Add the generated code to your PSN **'About Me'** and click **Verify Now**.\n3. Once verified you'll have full access to our server and state of the art Discord bot! Feel free to use the various slash commands.\n4. Visit our website! https://www.platpursuit.com/\nQuestions? Ping a moderator! {self.bot.plat_pursuit_emoji} {self.bot.platinum_emoji}",
                 inline=False,
             )
-            embed.set_footer(text=f"Happy pursuing! 🏆 | Powered by Plat Pursuit")
+            embed.set_footer(text=f"Happy pursuing! 🏆 | No Trophy Can Hide From Us")
 
             await channel.send(content=f"Welcome {mention}!", embed=embed)
             logger.info(f"Sent welcome to {member.id} in channel {channel.id}")
